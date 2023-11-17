bc28d986b6c2b3060af0ba63ab8e2462614b667f
Victoria <61987205+vickyluvs@users.noreply.github.com>
2023-11-17 10:22:38 -0500

    Created the showGreeting function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//function to show greeting to the user
+function showGreeting(response) {
+  // sentence element selected here
+  let sentence = document.getElementById("sentence");
 
+  //data receive from api
+  let greeting = response.data.greeting;
+  let language = response.data.language;
+
+  //clearing out the previous sentence
+  sentence.innerHTML = " ";
+
+  //displaying the new greeting
+  sentence.innerHTML = `${greeting} means Hello in ${language}`;
+}
+// axios fetching the url and data
+let url = `https://www.greetingsapi.com/random`;
+setInterval(() => {
+  axios.get(url).then(showGreeting);
+}, 3000);
